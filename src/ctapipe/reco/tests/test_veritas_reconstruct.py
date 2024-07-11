cbc39bca20b7b25fa7cd5e77015fb797c37d8a7d
ruoyushang <38991937+ruoyushang@users.noreply.github.com>
2024-07-11 14:30:06 +0100

    Add files via upload

--- src/ctapipe/reco/tests/test_veritas_reconstruct.py
+++ src/ctapipe/reco/tests/test_veritas_reconstruct.py
@@ -13,21 +13,23 @@ ctapipe_input = os.environ.get("CTAPIPE_SVC_PATH")
 
 subprocess.call(f'rm {ctapipe_output}/output_plots/*.png', shell=True)
 
-#telescope_type = 'MST_SCT_SCTCam'
-telescope_type = 'MST_MST_NectarCam'
-#telescope_type = 'MST_MST_FlashCam'
-#telescope_type = 'SST_1M_DigiCam'
-#telescope_type = 'SST_ASTRI_ASTRICam'
-#telescope_type = 'SST_GCT_CHEC'
-#telescope_type = 'LST_LST_LSTCam'
+telescope_type = []
+telescope_type += ['MST_SCT_SCTCam']
+#telescope_type += ['MST_MST_NectarCam']
+#telescope_type += ['MST_MST_FlashCam']
+#telescope_type += ['LST_LST_LSTCam']
+#telescope_type += ['SST_ASTRI_ASTRICam']
+#telescope_type += ['SST_GCT_CHEC']
+#telescope_type += ['SST_1M_DigiCam']
 
-#sim_files = 'sct_onaxis_train.txt'
+sim_files = 'sct_onaxis_train.txt'
 #sim_files = 'sct_onaxis_test.txt'
 #sim_files = 'sct_diffuse_all.txt'
 #sim_files = 'mst_onaxis_train.txt'
-sim_files = 'mst_onaxis_test.txt'
+#sim_files = 'mst_onaxis_test.txt'
 #sim_files = 'mst_diffuse_all.txt'
 
+ana_tag = 'clean_param_2_5_2'
 with open(f'{ctapipe_input}/{sim_files}', 'r') as file:
     for line in file:
         training_sample_path = get_dataset_path(line.strip('\n'))
@@ -36,6 +38,5 @@ with open(f'{ctapipe_input}/{sim_files}', 'r') as file:
         print (f"run_id = {run_id}")
         #if int(run_id)<=2000: continue
 
-        #loop_all_events(training_sample_path,ctapipe_output,telescope_type,save_output=True)
-        loop_all_events(training_sample_path,ctapipe_output,telescope_type,save_output=False)
+        loop_all_events(ana_tag,training_sample_path,ctapipe_output,telescope_type,save_output=True)
 
